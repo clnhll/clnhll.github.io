d007803db589986247558fa8ebd7a780c90fbd3f
clnh <chill392@gmail.com>
2015-06-22 20:20:45 +0000

    less fanciness

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var counter=0;
-var colors=['rgb(170,216,176)','rgb(255,204,92)','rgb(140,190,215)'];
+var colors=['rgb(170,216,176)','rgb(255,204,92)','rgb(140,190,245)'];
 colors.push('rgb(256,140,150)');
 function changeColor(){
   if (counter>colors.length-1) 
