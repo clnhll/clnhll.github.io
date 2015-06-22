ee576ffc64b910d5d5ff08a35255bd254d0a698f
clnh <chill392@gmail.com>
2015-06-22 20:17:24 +0000

    less fanciness

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 var counter=0;
-var colors=['#f0ad4e','rgb(256,140,150)'];
+var colors=['rgb(170,216,176)','rgb(255,204,92)','rgb(140,190,215)'];
+colors.push('rgb(256,140,150)');
 function changeColor(){
   if (counter>colors.length-1) 
   {counter=0;}
