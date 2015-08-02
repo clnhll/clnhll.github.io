6005f6d17029f5aa57e14ae01e8a981d51923ee8
clnh <chill392@gmail.com>
2015-08-02 23:23:29 +0000

    update home page

--- js/index.js
+++ js/index.js
@@ -1,12 +1,5 @@
-var counter=0;
-var colors=['rgb(170,216,176)','rgb(255,204,92)','rgb(140,190,245)'];
-colors.push('rgb(256,140,150)');
-function changeColor(){
-  if (counter>colors.length-1) 
-  {counter=0;}
-  less.modifyVars({
-      '@accent': colors[counter]   
-    });
-  counter++; 
-  less.refreshStyles();
-}
+  WebFont.load({
+    google: {
+      families: ['Asar']
+    }
+  });
