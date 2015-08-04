c3361cbe5efd2d19567b992f22f3d7fbee05df86
clnh <chill392@gmail.com>
2015-08-04 22:47:38 +0000

    updated links

--- js/index.js
+++ js/index.js
@@ -56,35 +56,35 @@ config(function($sceDelegateProvider) {
       link: "http://clnhll.com/todo/",
       img: "http://clnhll.com/todo.png",
       snippet: "I used this project to learn Angular Material, various JavaScript libraries, app caching and local storage. It features offline availability, multiple lists separated with Angular Material's dynamic tabs, drag-and-drop reordering of list items and persistent browser storage of list data.",
-      git: "https://github.com/clnhll/clnhll.github.io/tree/master/todo"  
+      src: "https://github.com/clnhll/clnhll.github.io/tree/master/todo"  
     },
     {
       title: "Pollarama",
       link: "http://polltastic.herokuapp.com/",
       img: "http://clnhll.com/polls.png",
       snippet: "My first MEAN full-stack project is a polling platform, and while aesthetically a little rough around the edges the groundwork is incredibly solid. Featuring Google charts, custom APIs, user authentication and persistent storage.",
-      git: "https://github.com/clnhll/polltastic" 
+      src: "https://github.com/clnhll/polltastic" 
     },
     {
       title: "Plans tonight?",
       link: "http://whatsgoinontonight.herokuapp.com/",
       img: "http://clnhll.com/tonight.png",
       snippet: "My second MEAN full-stack project is a simple social nightlife platform. Utilizes API keys from YELP to display a list of bars at your location, allows Twitter OAuth authenticated users to indicate whether they are attending or not, shows number of attendees, remembers authenticated users' last searched location.",
-      git: "https://github.com/clnhll/whatstonight" 
+      src: "https://github.com/clnhll/whatstonight" 
     },
     {
       title: "StockStream",
       link: "http://stockstream.herokuapp.com/",
       img: "http://clnhll.com/stocks.png",
       snippet: "My third MEAN full-stack project is a collaborative stock charting platform. It pulls stock information from Quandl and utilizes Socket.io to live-update the stock list across multiple clients without requiring a browser refresh.",
-      git: "https://github.com/clnhll/stockstream" 
+      src: "https://github.com/clnhll/stockstream" 
     },
     {
       title: "BookJump",
       link: "http://bookjump.herokuapp.com/",
       img: "http://clnhll.com/books.png",
       snippet: "My fourth MEAN full-stack project is a social book-sharing app. It allows users to share their libraries easily with one-another, and utilizes the Google Books API and the Google API Node.js client to get cover and title information.",
-      git: "https://github.com/clnhll/bookjump" 
+      src: "https://github.com/clnhll/bookjump" 
     },
     {
       title: "Angular Material Twitch.tv Buddy List",
@@ -92,7 +92,7 @@ config(function($sceDelegateProvider) {
       img: "http://clnhll.com/twitch.png",
       embed:"xGmLaq",
       snippet: "This project was an introduction to using a JSON API, which I later went and re-did using AngularJS and Angular Material for practice. It fetches a list of Twitch.tv usernames and displays some information about them.",
-      git: "https://github.com/clnhll/clnhll.github.io/tree/master/twitch" 
+      src: "http://codepen.io/cln/pen/xGmLaq" 
     },
     {
       title: "Angular Emoji Tic-Tac-Toe",
@@ -100,7 +100,7 @@ config(function($sceDelegateProvider) {
       img: "http://clnhll.com/tic-tac-toe.png",
       embed: "eNbRxo",
       snippet: "A totally unfair game of tic-tac-toe with an unbeatable AI written with AngularJS. The graphics in this game are emoji so for an optimal experience you should use Safari on a Mac. you may want to use an emoji-friendly browser. Computer lets you play first if you lose, computer plays first if you tie.",
-      git: "https://github.com/clnhll/clnhll.github.io/tree/master/tic-tac-toe" 
+      src: "http://codepen.io/cln/pen/eNbRxo" 
     },
     {
       title: "Wikipedia Live Search",
@@ -108,7 +108,7 @@ config(function($sceDelegateProvider) {
       img: "http://clnhll.com/wiki.png",
       embed: "ZGVXee",
       snippet: "A live-updating article title search using the Wikipedia JSON API, AngularJS and BootStrap. Clicking the random button will also display a random article on command.",
-      git: "https://github.com/clnhll/clnhll.github.io/tree/master/wiki" 
+      src: "http://codepen.io/cln/pen/ZGVXee" 
     },
     /** {
       title: "Local Weather",
@@ -124,7 +124,7 @@ config(function($sceDelegateProvider) {
       img: "http://clnhll.com/calc.png",
       embed: "GJmYKN",
       snippet: "This retro-style calculator is super cute and just as functional as any other calculator from the seventies. Allows chaining of operations just like a real calculator would. Is not solar powered, unfortunately.",
-      git: "https://github.com/clnhll/clnhll.github.io/tree/master/calc" 
+      src: "http://codepen.io/cln/pen/GJmYKN" 
     },
     {
       title: "Pomodoro Timer",
@@ -132,7 +132,7 @@ config(function($sceDelegateProvider) {
       img: "http://clnhll.com/pomo.png",
       embed: "zGwWQp",
       snippet: "A cute timer for the pomodoro scheduling program. Input work time and break time and watch as the circle fills up with seconds of your life you'll never get back. Bonus: plays an annoying noise when it gets to zero!",
-      git: "https://github.com/clnhll/clnhll.github.io/tree/master/pomodoro" 
+      src: "http://codepen.io/cln/pen/zGwWQp" 
     },
     {
       title: "Random Twitter API Abuser",
@@ -140,7 +140,7 @@ config(function($sceDelegateProvider) {
       img: "http://clnhll.com/tweets.png",
       embed: "NqjNZJ",
       snippet: "A rough and dirty hacked-together way of working around Twitter's API auth tokens and tweet display requirements and reading a user's tweets. Pushes an RSS feed of my friend @adamjk's tweets through an RSS to JSON filter and displays a random one at the press of a button.",
-      git: "https://github.com/clnhll/clnhll.github.io/tree/master/twitter" 
+      src: "http://codepen.io/cln/pen/zGwWQp" 
     }
   ];
 }
