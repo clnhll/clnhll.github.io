8b5aa05406bdf1063b0037a23b1bea23318e5954
Colin Hill <chill@iofficecorp.com>
2017-05-07 16:39:13 -0500

    update for https

--- js/index.js
+++ js/index.js
@@ -18,94 +18,94 @@ var vm = new Vue({
     projectData: [
       {
         title: "Angular Material Todo",
-        link: "http://clnhll.com/todo/",
-        img: "http://clnhll.com/todo.png",
+        link: "https://clnhll.com/todo/",
+        img: "https://clnhll.com/todo.png",
         snippet: "I used this project to learn Angular Material, various JavaScript libraries, app caching and local storage. It features offline availability, multiple lists separated with Angular Material's dynamic tabs, drag-and-drop reordering of list items and persistent browser storage of list data.",
         src: "https://github.com/clnhll/clnhll.github.io/tree/master/todo"  
       },
       {
         title: "Pollarama",
-        link: "http://polltastic.herokuapp.com/",
-        img: "http://clnhll.com/polls.png",
+        link: "https://polltastic.herokuapp.com/",
+        img: "https://clnhll.com/polls.png",
         snippet: "My first MEAN full-stack project is a polling platform featuring Google charts, custom APIs, user authentication and persistent storage.",
         src: "https://github.com/clnhll/polltastic" 
       },
       {
         title: "Plans tonight?",
-        link: "http://whatsgoinontonight.herokuapp.com/",
-        img: "http://clnhll.com/tonight.png",
+        link: "https://whatsgoinontonight.herokuapp.com/",
+        img: "https://clnhll.com/tonight.png",
         snippet: "My second MEAN full-stack project is a simple social nightlife platform. Utilizes API keys from YELP to display a list of bars at your location, allows Twitter OAuth authenticated users to indicate whether they are attending or not, shows number of attendees, remembers authenticated users' last searched location.",
         src: "https://github.com/clnhll/whatstonight" 
       },
       {
         title: "StockStream",
-        link: "http://stockstream.herokuapp.com/",
-        img: "http://clnhll.com/stocks.png",
+        link: "https://stockstream.herokuapp.com/",
+        img: "https://clnhll.com/stocks.png",
         snippet: "My third MEAN full-stack project is a collaborative stock charting platform. It pulls stock information from Quandl and utilizes Socket.io to live-update the stock list across multiple clients without requiring a browser refresh.",
         src: "https://github.com/clnhll/stockstream" 
       },
       {
         title: "BookJump",
-        link: "http://bookjump.herokuapp.com/",
-        img: "http://clnhll.com/books.png",
+        link: "https://bookjump.herokuapp.com/",
+        img: "https://clnhll.com/books.png",
         snippet: "My fourth MEAN full-stack project is a social book-sharing app. It allows users to share their libraries easily with one-another, and utilizes the Google Books API and the Google API Node.js client to get cover and title information.",
         src: "https://github.com/clnhll/bookjump" 
       },
       {
         title: "Angular Material Twitch.tv Buddy List",
-        link: "http://codepen.io/cln/pen/xGmLaq",
-        img: "http://clnhll.com/twitch.png",
+        link: "https://codepen.io/cln/pen/xGmLaq",
+        img: "https://clnhll.com/twitch.png",
         embed:"xGmLaq",
         snippet: "This project was an introduction to using a JSON API, which I later went and re-did using AngularJS and Angular Material for practice. It fetches a list of Twitch.tv usernames and displays some information about them.",
-        src: "http://codepen.io/cln/pen/xGmLaq" 
+        src: "https://codepen.io/cln/pen/xGmLaq" 
       },
       {
         title: "Angular Emoji Tic-Tac-Toe",
-        link: "http://codepen.io/cln/pen/eNbRxo",
-        img: "http://clnhll.com/tic-tac-toe.png",
+        link: "https://codepen.io/cln/pen/eNbRxo",
+        img: "https://clnhll.com/tic-tac-toe.png",
         embed: "eNbRxo",
         snippet: "A totally unfair game of tic-tac-toe with an unbeatable algorithm written with AngularJS. The graphics in this game are emoji so for an optimal experience you should use Safari on a Mac. Computer lets you play first if you lose, computer plays first if you tie.",
-        src: "http://codepen.io/cln/pen/eNbRxo" 
+        src: "https://codepen.io/cln/pen/eNbRxo" 
       },
       {
         title: "Wikipedia Live Search",
-        link: "http://codepen.io/cln/pen/ZGVXee",
-        img: "http://clnhll.com/wiki.png",
+        link: "https://codepen.io/cln/pen/ZGVXee",
+        img: "https://clnhll.com/wiki.png",
         embed: "ZGVXee",
         snippet: "A live-updating article title search using the Wikipedia API, AngularJS and BootStrap. Clicking the random button will also display a random article on command.",
-        src: "http://codepen.io/cln/pen/ZGVXee" 
+        src: "https://codepen.io/cln/pen/ZGVXee" 
       },
       {
         title: "Local Weather",
-        link: "http://codepen.io/cln/pen/vOmxJY",
-        img: "http://clnhll.com/weather.png",
+        link: "https://codepen.io/cln/pen/vOmxJY",
+        img: "https://clnhll.com/weather.png",
         embed: "vOmxJY",
         snippet: "A little weather applet, uses your browser's geolocation to retrieve your location, allows switching between imperial and metric. Displays different graphics based on temperature.",
-        src: "http://codepen.io/cln/pen/vOmxJY",
+        src: "https://codepen.io/cln/pen/vOmxJY",
       }, 
       {
         title: "Retro Calculator",
-        link: "http://codepen.io/cln/pen/GJmYKN",
-        img: "http://clnhll.com/calc.png",
+        link: "https://codepen.io/cln/pen/GJmYKN",
+        img: "https://clnhll.com/calc.png",
         embed: "GJmYKN",
         snippet: "This retro-style calculator is super cute and just as functional as any other calculator from the seventies. Allows chaining of operations just like a real calculator would. Is not solar powered, unfortunately.",
-        src: "http://codepen.io/cln/pen/GJmYKN" 
+        src: "https://codepen.io/cln/pen/GJmYKN" 
       },
       {
         title: "Pomodoro Timer",
-        link: "http://codepen.io/cln/pen/zGwWQp",
-        img: "http://clnhll.com/pomo.png",
+        link: "https://codepen.io/cln/pen/zGwWQp",
+        img: "https://clnhll.com/pomo.png",
         embed: "zGwWQp",
         snippet: "A cute timer for the pomodoro scheduling program. Input work time and break time and watch as the circle fills up with seconds of your life you'll never get back. Bonus: plays an annoying noise when it gets to zero!",
-        src: "http://codepen.io/cln/pen/zGwWQp" 
+        src: "https://codepen.io/cln/pen/zGwWQp" 
       // },
       // {
       //   title: "Random Twitter API Abuser",
-      //   link: "http://codepen.io/cln/pen/NqjNZJ",
-      //   img: "http://clnhll.com/tweets.png",
+      //   link: "https://codepen.io/cln/pen/NqjNZJ",
+      //   img: "https://clnhll.com/tweets.png",
       //   embed: "NqjNZJ",
       //   snippet: "A rough and dirty hacked-together way of working around Twitter's API auth tokens and tweet display requirements and reading a user's tweets. Pushes an RSS feed of a twitter user of your choice's tweets through an RSS to JSON filter and displays a random one at the press of a button.",
-      //   src: "http://codepen.io/cln/pen/NqjNZJ" 
+      //   src: "https://codepen.io/cln/pen/NqjNZJ" 
       }
     ],
     commandPressed: false
